f4de97979a21363fdc9eb269d6d94b393b38b319
Will Hall <hello@willhall.uk>
2021-04-13 18:37:00 +0100

    🚀 Test out if any client-side stuff is working.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
-import { init } from '../utils/sentry'
+import { init } from "../utils/sentry";
 
-init()
+import * as Sentry from "@sentry/node";
+
+init();
 
 export default function App({ Component, pageProps, err }) {
+  Sentry.captureMessage("Test Message for All Pages");
   // Workaround for https://github.com/vercel/next.js/issues/8592
-  return <Component {...pageProps} err={err} />
+  return <Component {...pageProps} err={err} />;
 }
